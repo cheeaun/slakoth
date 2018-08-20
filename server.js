825e9391ef4aa9f187c7f8b02f8d4821da356eda
Lim Chee Aun <cheeaun@gmail.com>
2018-08-21 00:25:05 +0800

    Fix times, more debugging and fixes

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 require('dotenv').config();
 const got = require('got');
+const spacetime = require('spacetime')
 const CronJob = require('cron').CronJob;
-
 const { WebClient } = require('@slack/client');
 const web = new WebClient(process.env.SLACK_TOKEN);
-
-// Inspired by https://github.com/samverschueren/tz-format
-// date object, timezone offset from UTC in hours
-function tzDate(d, offset) {
-  offset = offset && offset * 60;
-  d.setMinutes(d.getMinutes() + d.getTimezoneOffset() + offset);
-  return d;
-};
 
 var cycleIndex = 0;
 var cycle = function(list){
@@ -21,8 +13,8 @@ var cycle = function(list){
 };
 var webuildColors = ['#c11a18', '#e06149', '#228dB7', '#f1e9b4'];
 
-const nowDate = tzDate(new Date(), 8);
-const postEvents = async () => {
+const generateMessage = async () => {
+  const nowDate = spacetime.now('Asia/Singapore');
   const newEventsResponse = await got('http://api-webuild.7e14.starter-us-west-2.openshiftapps.com/events', {
     json: true,
   });
@@ -33,23 +25,25 @@ const postEvents = async () => {
     ...newEventsResponse.body.events,
     ...oldEventsResponse.body.events
   ].sort((a, b) => new Date(a.start_time) - new Date(b.start_time))
-    .filter(ev => {
-      const eventDate = tzDate(new Date(ev.start_time), 8);
-      return nowDate.toDateString() == eventDate.toDateString();
+    .filter((ev, i) => {
+      const eventDate = spacetime(ev.start_time);
+      eventDate.goto('Asia/Singapore');
+      if (i == 0) console.log(nowDate.format('iso-short'), '↔️ ', eventDate.format('iso-short'));
+      return nowDate.format('iso-short') == eventDate.format('iso-short');
     })
     .slice(0, 15);
 
   const attachments = events.map((event) => {
-    const [date, day, time] = event.formatted_time.split(',');
+    const dt = spacetime(event.start_time);
+    dt.goto('Asia/Singapore');
+    const datetime = dt.format('nice-day');
     const groupName = event.group_name.trim().replace(/\*/g, '٭­');
-    const formattedDate = date.trim().replace(/-/g, ' ').replace(/\s+\d{4}/, '');
-    const formattedTime = time.trim().replace(/^0/, '').toLowerCase();
     const location = event.location.trim().replace(/\*/g, '٭');
     return {
       title: event.name,
       title_link: event.url,
       color: cycle(webuildColors),
-      text: `by *${groupName}* on *${formattedDate}*, ${formattedTime}\n${location}`
+      text: `by *${groupName}* on *${datetime}\n${location}`
     };
   });
 
@@ -60,6 +54,11 @@ const postEvents = async () => {
     text: '😭 No events today from We Build SG https://webuild.sg/',
   };
 
+  return msg;
+}
+
+const postEvents = async () => {
+  const msg = await generateMessage();
   web.chat.postMessage({
     channel: process.env.SLACK_CHANNEL,
     ...msg,
@@ -70,13 +69,23 @@ const postEvents = async () => {
     .catch(console.error);
 };
 
-// postEvents();
-
 new CronJob('0 0 11 * * 1-6', postEvents, null, true, 'Asia/Singapore');
 
 const http = require('http');
-http.createServer((req, res) => {
-  res.setHeader('content-type', 'text/plain');
-  res.statusCode = 200;
-  res.end('https://github.com/cheeaun/slakoth');
+http.createServer(async (req, res) => {
+  if (req.url == '/'){
+    const msg = await generateMessage();
+    res.setHeader('content-type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(msg, null, '\t'));
+  } else if (req.url == '/post'){
+    await postEvents();
+    res.setHeader('content-type', 'text/plain');
+    res.statusCode = 200;
+    res.end('Posted message to channel');
+  } else {
+    res.setHeader('content-type', 'text/plain');
+    res.statusCode = 404;
+    res.end('404');
+  }
 }).listen(process.env.PORT || 1337);
